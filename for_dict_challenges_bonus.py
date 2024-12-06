37822a67be89be732ae736b27d2f5cdda8e5fb76
Anastasia Bespalenko <bluestaza@yandex.ru>
2024-12-06 11:45:35 +0300

    Готовая домашняя работа

--- for_dict_challenges_bonus.py
+++ for_dict_challenges_bonus.py
@@ -47,7 +47,7 @@ def generate_chat_history():
     for _ in range(messages_amount):
         sent_at += datetime.timedelta(minutes=random.randint(0, 240))
         messages.append({
-            "id": uuid.uuid4(),
+            "id": str(uuid.uuid4()),
             "sent_at": sent_at,
             "sent_by": random.choice(users_ids),
             "reply_for": random.choice(
@@ -67,4 +67,107 @@ def generate_chat_history():
 
 
 if __name__ == "__main__":
-    print(generate_chat_history())
+    generated_messages = generate_chat_history()
+    print(generated_messages)
+
+    users_id = []
+    for message in generated_messages:
+        users_id.append(message['sent_by'])
+    users_id = list(dict.fromkeys(users_id))
+
+    def count_largest(dictionary):
+        largest = 0
+        largest_item = 0
+        for key, value in dictionary.items():
+            if largest < value:
+                largest = value
+                largest_item = key
+            elif largest == value:
+                largest_item = f'{largest_item}, {key}'
+        return largest_item
+
+    def count_includes(info, key1, key2):
+        object_count = {}
+        if key2 == '':
+            key2 = key1
+        for object in info:
+            count = 0
+            searched_item = object[key1]
+            for item in info:
+                if searched_item == item[key2]:
+                    count += 1
+            object_count[object[key1]] = count
+        return object_count
+
+    def get_spammer_id(info):
+        message_count = count_includes(info, 'sent_by', '')
+        spammer_id = count_largest(message_count)
+        return spammer_id
+
+    def get_discussed_id(info):
+        replies_count = {}
+        for author in users_id:
+            replies_count[author] = 0
+        replied_messages = count_includes(info, 'id', 'reply_for')
+        for key, value in replied_messages.items():
+            for message in generated_messages:
+                if key == message['id']:
+                    author_id = message['sent_by']
+                    replies_count[author_id] += value
+        return count_largest(replies_count)
+
+    def get_mostseen_id(info):
+        messages_seen_by = {}
+        for id in users_id:
+            seen_by = []
+            for message in info:
+                if id == message['sent_by']:
+                    for item in message['seen_by']:
+                        seen_by.append(item)
+            seen_by = list(set(seen_by))
+            messages_seen_by[id] = len(seen_by)
+        return count_largest(messages_seen_by)
+
+    def get_popular_time(info):
+        messages_time = {
+            'утром': 0,
+            'днем': 0,
+            'вечером': 0
+        }
+        time_list12 = []
+        time_list12_18 = []
+        time_list18 = []
+        for message in info:
+            if int(message['sent_at'].strftime('%H')) < 12:
+                time_list12.append(message['id'])
+            elif 12 < int(message['sent_at'].strftime('%H')) <= 18:
+                time_list12_18.append(message['id'])
+            elif 18 < int(message['sent_at'].strftime('%H')):
+                time_list18.append(message['id'])
+        messages_time['утром'] = len(time_list12)
+        messages_time['днем'] = len(time_list12_18)
+        messages_time['вечером'] = len(time_list18)
+        return count_largest(messages_time)
+
+    def get_tred_id(info):
+        messages_id = {}
+        for message in info:
+            count = 0
+            id = message['id']
+            search_id = id
+            while True:
+                for item in info:
+                    if search_id == item['reply_for']:
+                        count += 1
+                        search_id = item['id']
+                break
+            messages_id[id] = count
+        return count_largest(messages_id)
+
+    print(f'Айди пользователя, который написал больше всех сообщений: {get_spammer_id(generated_messages)}')
+    print(f'Айди пользователя, на сообщения которого больше всего отвечали: {get_discussed_id(generated_messages)}')
+    print(f'Айди пользователей, сообщения которых видело больше всего уникальных пользователей: {get_mostseen_id(generated_messages)}')
+    print(f'В чате больше всего сообщений: {get_popular_time(generated_messages)}')
+    print(f'Идентификаторы сообщений, которые стали началом для самых длинных тредов (цепочек ответов): {get_tred_id(generated_messages)}')
+
+
